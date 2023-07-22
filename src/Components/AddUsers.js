2761f998d5c0c44088064603c372bef2e9425f4f
munir79 <jakirhossainmunir79@gmail.com>
2023-07-22 13:43:46 +0600

    addUser

---
+++ src/Components/AddUsers.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+
+const AddUsers = () => {
+    const [user,setUser]=useState({});
+    const handleAdduser=event=>{
+        event.preventDefault();
+        console.log(user);
+        fetch('http://localhost:5000/users',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(user)
+
+        })
+        .then(res=>res.json())
+        .then(data=>console.log(data));
+
+    }
+    const handleinputBlur=event=>{
+        const value=event.target.value;
+        const feild=event.target.name;
+        const newUser={...user}
+        newUser[feild]=value;
+        setUser(newUser);
+        }
+        
+   
+    return (
+        <div>
+         <h4> Please add a new users</h4>
+<form onSubmit={handleAdduser}>
+<input onBlur={handleinputBlur} type="text" name='name'placeholder='enter your name' /><br />
+<input onBlur={handleinputBlur} type="email" name="email" id="" placeholder='enter your email' /> <br />
+<button type="submit"> Add User</button>
+</form>
+        </div>
+    );
+};
+
+export default AddUsers;
